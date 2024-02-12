1293b4a9763075f9197e359f2b3c3a9b3439c1ef
adii <adityathakur6354@gmail.com>
2024-02-12 12:15:46 +0530

    connecting to mongodb Atlas

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// require ('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app = express();
+
+;(async () => {
+    try{
+        await mongoose.connect(`mongodb+srv://aditya:aditya123@cluster0.nnxzcxj.mongodb.net/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error In app.on :"+ error);
+            throw error
+        })
+        app.listen(process.env.PORT|| 8000,()=>{
+            console.log("server is started on port " + process.env.PORT);
+        })
+
+    }catch(error){
+        console.log("Error:" +  error);
+        throw error
+    }
+})()
+*/
