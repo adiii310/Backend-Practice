b22b637395eee5907d3d31ca59f0d634ed8dfb93
adii <adityathakur6354@gmail.com>
2024-02-23 10:37:06 +0530

    settingup multer and cloudinary to store images

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,34 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+import {v2 as Cloudinary} from "cloudinary"
+import fs from "fs"
 
-cloudinary.config({
-  cloud_name: process.envCLOUDINARY_CLOUD_NAME,
-  api_key: process.envCLOUDINARY_API_KEY,
-  api_secret: process.envCLOUDINARY_API_SECRET,
+
+Cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-  try {
-    if (!localFilePath) return null;
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await Cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
 
-    const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto",
-    });
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
 
-    console.log("File has been uploaded");
-    console.log(response.url);
 
-    return response;
-  } catch (error) {
-    fs.unlinkSync(localFilePath);
-    return null;
-  }
-};
 
-export { uploadOnCloudinary };
+export {uploadOnCloudinary}
 
 /*
 cloudinary.uploader.upload(
